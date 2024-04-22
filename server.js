c5deadaf473015e4f5c70aa554bc0a562996f0a8
Pankaj Kumar <pankajsing555@gmail.com>
2024-04-22 22:51:00 +0530

    add path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import Connection from "./database/db.js";
 import Routes from "./routes/route.js";
+import path from "path";
 
 dotenv.config();
 
